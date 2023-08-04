70cdb7cc7f51c85045b6f35d27d1738310963ef8
abugi <abugiboi@gmail.com>
2023-08-04 21:36:50 +0100

    feat: filter issues

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,35 +1,33 @@
-import { Link } from "react-router-dom";
-import { useQuery } from '@tanstack/react-query';
-import { IssueItem } from './IssueItem';
+import { useQuery } from "@tanstack/react-query";
+import { IssueItem } from "./IssueItem";
 
-export default function IssuesList() {
-  const issuesQuerry = useQuery(
-    ['issues'],
-    () => fetch('/api/issues').then(res => res.json())
-  )
-
-  const {isLoading, data} = issuesQuerry
-
+export default function IssuesList({ labels }) {
+  const issuesQuery = useQuery(["issues", { labels }], () => {
+    const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+    return fetch(`/api/issues?${labelsString}`).then((res) => res.json());
+  });
   return (
     <div>
-      <h1>Issues List</h1>
-      {
-        isLoading ? <p>Loading...</p> : <ul className="issues-list">
-        {
-          data.map(issue => <IssueItem
-            key={issue.id}
-            title={issue.title}
-            number={issue.number}
-            assignee={issue.assignee}
-            commentCount={issue.comments.length}
-            createdBy={issue.createdBy}
-            createdDate={issue.createdDate}
-            labels={issue.labels}
-            status={issue.status}
-          />)
-        }
-      </ul>
-      }
+      <h2>Issues List</h2>
+      {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="issues-list">
+          {issuesQuery.data.map((issue) => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              assignee={issue.assignee}
+              commentCount={issue.comments.length}
+              createdBy={issue.createdBy}
+              createdDate={issue.createdDate}
+              labels={issue.labels}
+              status={issue.status}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
