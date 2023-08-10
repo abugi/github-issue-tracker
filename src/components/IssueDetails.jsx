5ffbfb4336ceafb4140ca417d0fbb558ff624746
abugi <abugiboi@gmail.com>
2023-08-10 17:55:15 +0100

    feat: issue labels, assignment and status

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,8 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
 import { IssueHeader } from "./IssueHeader";
-import { Comment } from './Comment';
-import { useIssueData } from '../helpers/useIssueData';
-import { useIssueComments } from '../helpers/useIssueComments';
+import IssueLabels from "./IssueLabels";
+import IssueStatus from "./IssueStatus";
+import IssueAssignment from './IssueAssignment';
+
+function useIssueData(issueNumber) {
+  return useQuery(["issues", issueNumber], ({ signal }) => {
+    return fetch(`/api/issues/${issueNumber}`, { signal }).then((res) =>
+      res.json()
+    );
+  });
+}
+
+function useIssueComments(issueNumber) {
+  return useQuery(["issues", issueNumber, "comments"], ({ signal }) => {
+    return fetch(`/api/issues/${issueNumber}/comments`, { signal }).then(
+      (res) => res.json()
+    );
+  });
+}
+
+function Comment({ comment, createdBy, createdDate }) {
+  const userQuery = useUserData(createdBy);
+
+  if (userQuery.isLoading)
+    return (
+      <div className="comment">
+        <div>
+          <div className="comment-header">Loading...</div>
+        </div>
+      </div>
+    );
+
+  return (
+    <div className="comment">
+      <img src={userQuery.data.profilePictureUrl} alt="Commenter Avatar" />
+      <div>
+        <div className="comment-header">
+          <span>{userQuery.data.name}</span> commented{" "}
+          <span>{relativeDate(createdDate)}</span>
+        </div>
+        <div className="comment-body">{comment}</div>
+      </div>
+    </div>
+  );
+}
 
 export default function IssueDetails() {
   const { number } = useParams();
@@ -27,7 +72,20 @@ export default function IssueDetails() {
                 ))
               )}
             </section>
-            <aside></aside>
+            <aside>
+              <IssueStatus
+                status={issueQuery.data.status}
+                issueNumber={issueQuery.data.number.toString()}
+              />
+              <IssueAssignment
+                assignee={issueQuery.data.assignee}
+                issueNumber={issueQuery.data.number.toString()}
+              />
+              <IssueLabels
+                labels={issueQuery.data.labels}
+                issueNumber={issueQuery.data.number.toString()}
+              />
+            </aside>
           </main>
         </>
       )}
