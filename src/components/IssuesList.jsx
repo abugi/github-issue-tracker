ace322b2e2417b3d191b5ce7e300dc03f837702e
abugi <abugiboi@gmail.com>
2023-08-11 10:13:46 +0100

    perf: data pagination

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,27 +1,43 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { IssueItem } from "./IssueItem";
 import fetchWithError from "../helpers/fetchWithError";
-import Loader from './Loader';
+import Loader from "./Loader";
 
-export default function IssuesList({ labels, status }) {
+export default function IssuesList({ labels, status, pageNum, setPageNum }) {
+  const queryClient = useQueryClient();
   const issuesQuery = useQuery(
-    ["issues", { labels, status }],
-    ({signal}) => {
+    ["issues", { labels, status, pageNum }],
+    async ({ signal }) => {
       const statusString = status ? `&status=${status}` : "";
       const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
-      return fetchWithError(`/api/issues?${labelsString}${statusString}`, {signal});
+      const paginationString = pageNum ? `&page=${pageNum}` : "";
+
+      const results = await fetchWithError(
+        `/api/issues?${labelsString}${statusString}${paginationString}`,
+        {
+          signal,
+        }
+      );
+
+      results.forEach((issue) => {
+        queryClient.setQueryData(["issues", issue.number.toString()], issue);
+      });
+
+      return results;
     },
     {
-      staleTime: 1000 * 60,
+      keepPreviousData: true,
     }
   );
   const [searchValue, setSearchValue] = useState("");
 
   const searchQuery = useQuery(
     ["issues", "search", searchValue],
-    () =>
-      fetch(`/api/search/issues?q=${searchValue}`).then((res) => res.json()),
+    ({ signal }) =>
+      fetch(`/api/search/issues?q=${searchValue}`, { signal }).then((res) =>
+        res.json()
+      ),
     {
       enabled: searchValue.length > 0,
     }
@@ -48,28 +64,62 @@ export default function IssuesList({ labels, status }) {
           }}
         />
       </form>
-      <h2>Issues List {issuesQuery.fetchStatus === "fetching" ? <Loader /> : null}</h2>
+      <h2>
+        Issues List {issuesQuery.fetchStatus === "fetching" ? <Loader /> : null}
+      </h2>
       {issuesQuery.isLoading ? (
         <p>Loading...</p>
       ) : issuesQuery.isError ? (
         <p>{issuesQuery.error.message}</p>
       ) : searchQuery.fetchStatus === "idle" &&
         searchQuery.isLoading === true ? (
-        <ul className="issues-list">
-          {issuesQuery.data.map((issue) => (
-            <IssueItem
-              key={issue.id}
-              title={issue.title}
-              number={issue.number}
-              assignee={issue.assignee}
-              commentCount={issue.comments.length}
-              createdBy={issue.createdBy}
-              createdDate={issue.createdDate}
-              labels={issue.labels}
-              status={issue.status}
-            />
-          ))}
-        </ul>
+        <>
+          <ul className="issues-list">
+            {issuesQuery.data.map((issue) => (
+              <IssueItem
+                key={issue.id}
+                title={issue.title}
+                number={issue.number}
+                assignee={issue.assignee}
+                commentCount={issue.comments.length}
+                createdBy={issue.createdBy}
+                createdDate={issue.createdDate}
+                labels={issue.labels}
+                status={issue.status}
+              />
+            ))}
+          </ul>
+          <div className="pagination">
+            <button
+              onClick={() => {
+                if (pageNum - 1 > 0) {
+                  setPageNum(pageNum - 1);
+                }
+              }}
+              disabled={pageNum === 1}
+            >
+              Previous
+            </button>
+            <p>
+              Page {pageNum} {issuesQuery.isFetching ? "..." : ""}
+            </p>
+            <button
+              disabled={
+                issuesQuery.data?.length === 0 || issuesQuery.isPreviousData
+              }
+              onClick={() => {
+                if (
+                  issuesQuery.data?.length !== 0 &&
+                  !issuesQuery.isPreviousData
+                ) {
+                  setPageNum(pageNum + 1);
+                }
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </>
       ) : (
         <>
           <h2>Search Results</h2>
